c44195bfed1deba63785daf5940673aba2dde7a4
Rajesh <rajeshkumartharu959@gmail.com>
2024-12-18 12:48:25 +0545

    adding a README File to the repository

---
+++ ClassObject/bankAccount.py
@@ -0,0 +1,28 @@
+import random
+
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+        print("Welcome to Simple Deposite and Withdraw Machine")
+
+    def deposite(self):
+        amount = float(input("Enter amount to be Deposited:"))
+        self.balance += amount
+        print("\n Deposited amount:",amount)
+
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdraw:"))
+
+        if self.balance>=amount:
+            self.balance -= amount
+            print("\n Withdraw amount:", amount)
+        else:
+            print("\n Insufficient balance")
+
+    def check_balance(self):
+        print("\n Net Available Balance:",self.balance)
+
+bankaccount = BankAccount()
+bankaccount.deposite()
+bankaccount.withdraw()
+bankaccount.check_balance()
